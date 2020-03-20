bb97d5ee8bd71c1d877349ef5319cdbb0dd181ec
valeriocristofori <cristoforivalerio@gmail.com>
2020-03-20 10:33:22 +0100

    added signup GUI, signup.xml great model of a ConstraintLayout

--- TheFridgeMobile/app/src/main/java/it/valeriocristofori/thefridgemobile/controller/MainActivity.java
+++ TheFridgeMobile/app/src/main/java/it/valeriocristofori/thefridgemobile/controller/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import it.valeriocristofori.thefridgemobile.R;
 
@@ -18,9 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //init login button
-        Button btn = (Button)findViewById(R.id.btn_login);
+        Button btn_login = (Button)findViewById(R.id.btn_login);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        //init signup button
+        Button btn_signup = (Button)findViewById(R.id.btn_signup);
+
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //take text from edit texts
@@ -29,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
                 //goto home GUI
                 startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //remand to register GUI
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
 
