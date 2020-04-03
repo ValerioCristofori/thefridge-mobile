b8496a9d0614ff642a6b4a84bdc1990bd08cd505
valeriocristofori <cristoforivalerio@gmail.com>
2020-04-03 21:15:56 +0200

    implemented remember me and refactor sign in/up Guis

--- TheFridgeMobile/app/src/main/java/it/valeriocristofori/thefridgemobile/activity/sign/MainActivity.java
+++ TheFridgeMobile/app/src/main/java/it/valeriocristofori/thefridgemobile/activity/sign/MainActivity.java
@@ -3,12 +3,16 @@ package it.valeriocristofori.thefridgemobile.activity.sign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,38 +30,70 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Food> listFood;
     private DatabaseHelper db;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = DatabaseHelper.getDatabaseInstance(getApplicationContext());
+
         //create this.Holder
         Holder holder = new Holder();
 
-        db = DatabaseHelper.getDatabaseInstance(getApplicationContext());
+
 
     }
 
-    class Holder implements View.OnClickListener{
+    class Holder implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
         private EditText tfUsername;
         private EditText tfPassword;
         private Button btnLogin;
         private Button btnSignup;
+        private CheckBox cbRemember;
 
         public Holder(){
             //init login button
-            this.btnLogin = (Button)findViewById(R.id.btnLogin);
+            this.btnLogin = findViewById(R.id.btnLogin);
             //init sign up button
-            this.btnSignup = (Button)findViewById(R.id.btnSignup);
+            this.btnSignup = findViewById(R.id.btnSignup);
             //binding xml components and java components
-            this.tfUsername = (EditText)findViewById(R.id.tfUsername);
-            this.tfPassword = (EditText)findViewById(R.id.tfPassword);
+            this.tfUsername = findViewById(R.id.tfUsername);
+            this.tfPassword = findViewById(R.id.tfPassword);
+            this.cbRemember = findViewById(R.id.cbRemember);
+
+            //try to remember me status
+            preferences = getSharedPreferences( "checkbox", MODE_PRIVATE);
+            editor = preferences.edit();
+            this.trylogin();
 
             //assign click listener
             this.btnLogin.setOnClickListener(this);
             this.btnSignup.setOnClickListener(this);
+            this.cbRemember.setOnCheckedChangeListener(this);
 
+        }
+
+        private void trylogin() {
+            String res = preferences.getString("remember", "");
+            if( res.equals("true")){
+                //automatic login
+                String username = preferences.getString("username", "");
+                String password = preferences.getString("password", "");
+
+                Log.e("username", username);
+                Log.e("password", password);
+
+                LoginController loginController = new LoginController();
+                if( loginController.isValidUser( username, password ) ) {
+                    //remand home GUI
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+                }
+             }
         }
 
         @Override
@@ -80,6 +116,14 @@ public class MainActivity extends AppCompatActivity {
                 }
                 LoginController loginController = new LoginController();
                 if( loginController.isValidUser( tfUsername.getText().toString(), tfPassword.getText().toString() )) {
+
+                    if(preferences.getString("remember", "").equals("true")){ //if the checkbox is checked
+                        //put username and pass in the shared preferences
+                        //not good in the point of view of security ---> pass not in md5()
+                        editor.putString( "username", tfUsername.getText().toString() );
+                        editor.putString( "password", tfPassword.getText().toString() );
+                        editor.commit();
+                    }
 
                     //remand home GUI
                     startActivity(new Intent(MainActivity.this, HomeActivity.class));
@@ -115,6 +159,20 @@ public class MainActivity extends AppCompatActivity {
             toast.setView(layout);
             toast.show();
 
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if( buttonView.isChecked() ){
+
+                editor.putString( "remember", "true");
+                editor.commit();
+
+            }else{
+
+                editor.putString( "remember", "false");
+                editor.commit();
+            }
         }
     }
 
