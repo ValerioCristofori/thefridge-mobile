589ab0a540f4ddee9873e3e2ca0ef659af421379
Valerio Cristofori <cristoforivalerio@gmail.com>
2020-06-27 19:10:49 +0200

    refactor --> add a thread for searching recipes and must refactor animation

--- TheFridgeMobile/app/src/main/java/it/Trilogia/thefridgemobile/model/customize/animation/AnimationCustom.java
+++ TheFridgeMobile/app/src/main/java/it/Trilogia/thefridgemobile/model/customize/animation/AnimationCustom.java
@@ -29,25 +29,7 @@ public class AnimationCustom {
             myView.setVisibility(View.INVISIBLE);
         }
 
-        // Check if the runtime version is at least Lollipop
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // get the center for the clipping circle
-            int cx = myView.getWidth() / 2;
-            int cy = myView.getHeight() / 2;
-
-            // get the final radius for the clipping circle
-            float finalRadius = (float) Math.hypot(cx, cy);
-
-            // create the animator for this view (the start radius is zero)
-            Animator anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0f, finalRadius);
-
-            // make the view visible and start the animation
-            myView.setVisibility(View.VISIBLE);
-            anim.start();
-        } else {
-            // set the view to invisible without a circular reveal animation below Lollipop
-            myView.setVisibility(View.INVISIBLE);
-        }
+        
 
     }
 }
